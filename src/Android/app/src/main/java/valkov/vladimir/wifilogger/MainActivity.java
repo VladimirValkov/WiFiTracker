ea647d2fec4d34baca370d7405c3bda0a1994adc
VladimirValkov <57315074+VladimirValkov@users.noreply.github.com>
2022-09-25 19:56:02 +0300

    created permission requests and test wifi scanner

--- src/Android/app/src/main/java/valkov/vladimir/wifilogger/MainActivity.java
+++ src/Android/app/src/main/java/valkov/vladimir/wifilogger/MainActivity.java
@@ -1,13 +1,81 @@
 package valkov.vladimir.wifilogger;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                Log.d("vlad", "receive");
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (success) {
+                    //scanSuccess();
+                    Log.d("vlad", "results1");
+                    List<ScanResult> results = wifiManager.getScanResults();
+                    Log.d("vlad", "results2->"+ results.size());
+                    for (ScanResult res: results) {
+                        text.append("\n" + res.toString());
+                    }
+                } else {
+                    // scan failure handling
+                    Log.d("vlad", "error2");
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        this.getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean success = wifiManager.startScan();
+                if (!success) {
+                    // scan failure handling
+                    //scanFailure();
+                    Log.d("vlad", "error");
+                }
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION }, 9999);
+        }
+
     }
+
 }
