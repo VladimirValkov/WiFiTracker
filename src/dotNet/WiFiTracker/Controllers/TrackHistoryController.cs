1bcc9f6447f6b1903c1a9c7bfc7e8ead3becc7a7
VladimirValkov <57315074+VladimirValkov@users.noreply.github.com>
2022-11-13 22:41:02 +0200

    updated reports

--- src/dotNet/WiFiTracker/Controllers/TrackHistoryController.cs
+++ src/dotNet/WiFiTracker/Controllers/TrackHistoryController.cs
@@ -17,9 +17,9 @@ namespace WiFiTracker.Controllers
         }
         public IActionResult Index()
         {
-            var data = db.Points.Join(db.Terminals, p=>p.TerminalId, t=>t.Id, (p,t) => new MapPin()
+            var data = db.Points.OrderBy(a=>a.LogDate).Join(db.Terminals, p=>p.TerminalId, t=>t.Id, (p,t) => new MapPin()
                 {
-                    Name = t.Name,
+                    Name = t.Name + " - " + p.LogDate.ToString("dd.MM.yyyy HH:mm:ss"),
                     Latitude = p.Latitude.ToString().Replace(',', '.'),
                     Longitude = p.Longitude.ToString().Replace(',', '.'),
                 }
